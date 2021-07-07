0e8af9752b57b2a1ee150ba5d795d735adaee529
rodrigortigosa <rodrigortigosa@gmail.com>
2021-07-07 18:52:20 -0300

    Agrego clases de bootstrap para responsive en el componente Article. Ahora la descripcion del articulo se ve abajo de la imagen en desktop.

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,5 +1,4 @@
 import { Col, Image } from "react-bootstrap";
-import "../assets/css/Article.css";
 
 const Article = (props) => {
   const image = props.image;
@@ -8,17 +7,17 @@ const Article = (props) => {
 
   return (
     <Col className="mb-3">
-      <div className="d-flex justify-content-center">
-        <div className="div-img-article">
+      <div className="d-flex flex-md-column">
+        <div className="div-img-article w-100">
           <Image
-            className="img-article"
+            className="img-article w-100"
             src={image}
             alt={`imagen del ${name}`}
           ></Image>
         </div>
-        <div className="article-description d-flex flex-column">
+        <div className="article-description w-100 text-left ml-3 ml-md-0 mt-md-2 d-flex flex-column">
           <span className="article-name">{name}</span>
-          <span className="article-price mt-3">{`$${price}`}</span>
+          <span className="article-price mt-3 mt-md-2">{`$${price}`}</span>
         </div>
       </div>
     </Col>
