3e793b3a3f6bac7059417f9c84a5e85b9d3cb618
rodrigortigosa <rodrigortigosa@gmail.com>
2021-07-07 16:32:13 -0300

    Agrego estilo al componente Article

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,4 +1,5 @@
 import { Col, Image } from "react-bootstrap";
+import "../assets/css/Article.css";
 
 const Article = (props) => {
   const image = props.image;
@@ -6,13 +7,19 @@ const Article = (props) => {
   const price = props.price;
 
   return (
-    <Col className="d-flex justify-content-center my-3">
-      <div>
-        <Image src={image} alt={`imagen del ${name}`}></Image>
-      </div>
-      <div>
-        <p>{name}</p>
-        <p>{`$${price}`}</p>
+    <Col className="my-3">
+      <div className="d-flex justify-content-center">
+        <div className="div-img-article">
+          <Image
+            className="img-article"
+            src={image}
+            alt={`imagen del ${name}`}
+          ></Image>
+        </div>
+        <div className="article-description d-flex flex-column">
+          <span className="article-name">{name}</span>
+          <span className="article-price mt-3">{`$${price}`}</span>
+        </div>
       </div>
     </Col>
   );
