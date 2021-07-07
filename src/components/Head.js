fb0e9e23f83aed5fe9878b64f91cb9166785101e
rodrigortigosa <rodrigortigosa@gmail.com>
2021-07-07 18:14:26 -0300

    Agrego responsive para el componente Head

--- src/components/Head.js
+++ src/components/Head.js
@@ -4,12 +4,12 @@ import "../assets/css/Head.css";
 
 const Head = () => {
   return (
-    <Row className="justify-content-center row-cols-1 row-cols-md-3 mt-3 mb-4">
-      <Col md={4} className="text-center mb-4">
+    <Row className="justify-content-center justify-content-md-between row-cols-1 row-cols-md-3 mt-3 mb-4">
+      <Col md={4} lg={3} className="text-center text-md-left mb-4 mb-md-0">
         <Path departament="Juguetes" category="Funko Pop" />
       </Col>
 
-      <Col md={4} className="d-flex justify-content-center mb-2">
+      <Col md={4} lg={3} className="d-flex justify-content-center mb-2 mb-md-0">
         <select id="select-filter" name="select-filter">
           <option value="title">Filtrar</option>
           <option value="opcion1">Filtro 1</option>
@@ -18,7 +18,11 @@ const Head = () => {
         </select>
       </Col>
 
-      <Col md={4} className="d-flex justify-content-center">
+      <Col
+        md={4}
+        lg={3}
+        className="d-flex justify-content-center justify-content-md-end"
+      >
         <select id="select-order" name="select-order">
           <option value="title">Ordenar por</option>
           <option value="opcion1">Opción 1</option>
