c5562783a20873e75eafc0e1405e1335d4232003
rodrigortigosa <rodrigortigosa@gmail.com>
2021-07-06 23:35:47 -0300

    Agrego componente Ruta

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
-function App() {
-  return <div className="App"></div>;
-}
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import Ruta from "./components/Ruta";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Container fluid>
+        <Ruta />
+
+        {/* Imagen */}
+
+        {/* <Listado></Listado> */}
+      </Container>
+    </div>
+  );
+};
 
 export default App;
