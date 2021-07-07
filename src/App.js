6303c17b06dfe52369b5ec4a95917d034a230b09
rodrigortigosa <rodrigortigosa@gmail.com>
2021-07-06 23:59:32 -0300

    Corrijo componente Ruta

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col, DropdownButton, Dropdown } from "react-bootstrap";
 import Ruta from "./components/Ruta";
 import Banner from "./components/Banner";
 
@@ -7,11 +7,35 @@ const App = () => {
   return (
     <div className="App">
       <Container fluid>
-        <Ruta />
+        <Row className="justify-content-between row-cols-1 row-cols-md-3">
+          <Col>
+            <Ruta />
+          </Col>
 
+          <Col>
+            <DropdownButton id="dropdown-item-button" title="Dropdown button">
+              <Dropdown.ItemText>Dropdown item text</Dropdown.ItemText>
+              <Dropdown.Item as="button">Action</Dropdown.Item>
+              <Dropdown.Item as="button">Another action</Dropdown.Item>
+              <Dropdown.Item as="button">Something else</Dropdown.Item>
+            </DropdownButton>
+          </Col>
+
+          <Col>
+            <DropdownButton id="dropdown-item-button" title="Dropdown button">
+              <Dropdown.ItemText>Dropdown item text</Dropdown.ItemText>
+              <Dropdown.Item as="button">Action</Dropdown.Item>
+              <Dropdown.Item as="button">Another action</Dropdown.Item>
+              <Dropdown.Item as="button">Something else</Dropdown.Item>
+            </DropdownButton>
+          </Col>
+        </Row>
         <Banner />
 
         {/* <Listado></Listado> */}
+        <Row className="row-cols-1 row-cols-md-3">
+          <Col></Col>
+        </Row>
       </Container>
     </div>
   );
--- src/components/Ruta.js
+++ src/components/Ruta.js
@@ -1,32 +1,8 @@
-import { Row, Col, DropdownButton, Dropdown } from "react-bootstrap";
-
 const Ruta = () => {
   return (
-    <Row className="justify-content-between row-cols-1 row-cols-md-3">
-      <Col>
-        <span>
-          Inicio / Juguetes / <b>Funko Pop</b>
-        </span>
-      </Col>
-
-      <Col>
-        <DropdownButton id="dropdown-item-button" title="Dropdown button">
-          <Dropdown.ItemText>Dropdown item text</Dropdown.ItemText>
-          <Dropdown.Item as="button">Action</Dropdown.Item>
-          <Dropdown.Item as="button">Another action</Dropdown.Item>
-          <Dropdown.Item as="button">Something else</Dropdown.Item>
-        </DropdownButton>
-      </Col>
-
-      <Col>
-        <DropdownButton id="dropdown-item-button" title="Dropdown button">
-          <Dropdown.ItemText>Dropdown item text</Dropdown.ItemText>
-          <Dropdown.Item as="button">Action</Dropdown.Item>
-          <Dropdown.Item as="button">Another action</Dropdown.Item>
-          <Dropdown.Item as="button">Something else</Dropdown.Item>
-        </DropdownButton>
-      </Col>
-    </Row>
+    <span>
+      Inicio / Juguetes / <b>Funko Pop</b>
+    </span>
   );
 };
 
